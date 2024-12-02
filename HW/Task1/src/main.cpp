3be4ba8c05a707bebfbf1992efd3c475c20b3546
Михаил Беляков <misa.belyakov.ru@gmail.com>
2024-12-02 19:40:33 +0700

    Task1 done

--- HW/Task1/src/main.cpp
+++ HW/Task1/src/main.cpp
@@ -2,41 +2,24 @@
 #include <vector>
 using namespace std;
 
-void maxSubarrayIndices(vector<int> arr, int start, int end) {
-    if (arr.empty()) {
-        start = -1;
-        end = -1;
-        return; //-1, -1 если массив пуст
-    }
-
-    int max_sum = arr[0];
-    int current_sum = 0;
-    start = 0;
-    end = 0;
-    int temp_start = 0;
-
-    for (int i = 0; i < arr.size(); ++i) {
-        current_sum += arr[i];
-
-        if (current_sum > max_sum) {
-            max_sum = current_sum;
-            start = temp_start;
-            end = i;
-        }
-
-        if (current_sum < 0) {
-            current_sum = 0;
-            temp_start = i + 1;
+int maxSubarrayIndexes(vector<int> arr) {
+    int i = 0, j = 0, iMax = 0, jMax = 0, sumMax = 0, sumTemp = 0;
+    for (; i < arr.size(); i++) {
+        sumTemp = arr[i];
+        for (int j = i + 1; j < arr.size(); ++j) {
+            sumTemp += arr[j];
+            if (sumTemp > sumMax) {
+                iMax = i;
+                jMax = j;
+                sumMax = sumTemp;
+            }
         }
     }
+    cout << "Indexes: " << iMax << ", " << jMax << "\nSum:\t" << sumMax;
 }
 
 int main() {
-    vector<int> a = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    
-    int start, end;
-    maxSubarrayIndices(a, start, end);
-    
-    cout << start << " " << end << endl;
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    maxSubarrayIndexes(arr);
     return 0;
 }

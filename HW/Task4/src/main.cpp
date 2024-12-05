6dfe65b95d24b2083fa6c6d04a2359f0ee2a0eb5
Михаил Беляков <misa.belyakov.ru@gmail.com>
2024-12-06 00:20:31 +0700

    Task4 reworked

--- HW/Task4/src/main.cpp
+++ HW/Task4/src/main.cpp
@@ -2,20 +2,17 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-
-bool compare(int a, int b){
-    return abs(a) < abs(b);
-}
-
-int main(){
-
-
-
-    vector<int> vec = {-100, -50, -5, 1, 10, 15};
-    
-    sort(vec.begin(), vec.end(), compare);
-
-    for(int num : vec) cout << num << "\t";
-
+int main()
+{
+    vector <int> vec = {-100,-50, -5, 1, 10, 15};
+    int right = 0;
+    while (right < vec.size() && vec[right] < 0) ++right;
+    int left = right - 1;
+    while (left >= 0 && right < vec.size()){
+        if (vec[right] > -vec[left]) cout << vec[left--] << " ";
+        else cout << vec[right++] << " ";
+    }
+    while (left >= 0) cout << vec[left--] << " "; // выводим левый хвост
+    while (right < vec.size()) cout << vec[right++] << " "; // выводим правый хвост
+    cout << endl;
 }
